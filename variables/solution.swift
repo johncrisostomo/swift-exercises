1007a00bb83602d5f6372df5cf222177e3c2357a
John Crisostomo <jd@snapzio.com>
2017-03-28 08:58:32 +0800

    finishes variables exercise

---
+++ variables/solution.swift
@@ -0,0 +1,41 @@
+// one
+var fifteen = 15
+
+// two
+var five = 5
+
+// three
+var sum = fifteen + five
+
+// four
+var product = 5 * 9
+
+// five
+var difference = fifteen - five
+
+// six 
+var quotient = fifteen / five
+
+// seven
+var one = 1, two = 2, three = 3
+
+// eight
+let const1 = 1, const2 = 2, const3 = 3
+
+// nine
+var doubleVar: Double
+
+// ten
+var intVar: Int = 423
+
+// eleven
+var first: String = "Hello"
+
+// twelve 
+var second: String = "World"
+
+// thirteen
+var greet: String = first + second
+
+// fourteen 
+var greet2: String = "/(first) /(second)"
