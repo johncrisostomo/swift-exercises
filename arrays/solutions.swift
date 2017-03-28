35e7363643c410b8b8650345c3c7fa2200524aee
John Crisostomo <jd@snapzio.com>
2017-03-28 10:07:05 +0800

    finishes arrays

---
+++ arrays/solutions.swift
@@ -0,0 +1,21 @@
+var stringArray = [String]()
+var doubleArray: [Double] = [12.400, 332.12312, 45.12321, 657.23421]
+var inferenceWay = [1, 2, 3]
+
+stringArray.append("Hello")
+
+doubleArray.append(3.1416)
+
+inferenceWay.append(4)
+
+stringArray.remove(at: 0)
+
+doubleArray.remove(at: 4)
+
+inferenceWay.remove(at: 3)
+
+doubleArray.removeAll()
+
+var firstNames = ["John", "Deejay", "Erick"]
+
+var allNames = firstNames + ["Crisostomo", "Geroso", "Maddumma"]
